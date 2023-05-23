ecd2bb28b3f33d4aa3f48c46917bac624373eb09
VJLYDER <134377948+VJLYDER@users.noreply.github.com>
2023-05-23 14:38:56 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,36 @@
+package com.dedalus;
+
+public class MyClass {
+	
+	public void name_one() {
+		
+		System.out.println("two");
+		
+	}
+	
+	public void name_two() {
+		
+		System.out.println("three");
+		
+	}
+	
+
+	public static void main(String[] args) {
+		
+
+		int x = 10;
+	    int y = 10;
+		
+		MyClass mc = new MyClass();
+		
+		
+		mc.name_one();
+		mc.name_two();
+		
+	}
+
+	
+	
+}
+
+
